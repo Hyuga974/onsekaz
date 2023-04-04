ed9abd23d192448d0f89456591d0ff43391f4651
lack <phuss.pro@gmail.com>
2023-04-04 11:13:58 +0200

    changed mongo to mysql

--- api-onsekaz/src/app.module.ts
+++ api-onsekaz/src/app.module.ts
@@ -3,17 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { appConfig } from './config/app.config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { databaseConfig } from './config/database.config';
 
 @Module({
-  imports: [ConfigModule.forRoot(appConfig),
-  MongooseModule.forRoot(databaseConfig.uri, {
-    useNewUrlParser: databaseConfig.useNewUrlParser,
-    useUnifiedTopology: databaseConfig.useUnifiedTopology,
-    useFindAndModify: databaseConfig.useFindAndModify,
-    useCreateIndex: databaseConfig.useCreateIndex
-  })],
+  imports: [ConfigModule.forRoot(appConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
