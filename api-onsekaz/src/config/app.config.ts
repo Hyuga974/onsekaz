b3e8886e0a87217739a1f31ba80d1db3ec2872e3
lack <phuss.pro@gmail.com>
2023-04-24 09:17:31 +0200

    nest to express

--- api-onsekaz/src/config/app.config.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigModuleOptions } from '@nestjs/config';
-
-export const appConfig: ConfigModuleOptions = {
-  envFilePath: ['.env', '.env.development'],
-  isGlobal: true,
-  cache: true,
-  expandVariables: true,
-};
