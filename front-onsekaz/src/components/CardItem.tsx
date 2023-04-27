9df8c85cdb2292adfab8eb5956a589e97d0d608e
Hyuga974 <tamashiyami@gmail.com>
2023-04-27 17:54:50 +0200

    add link button in annonce

--- front-onsekaz/src/components/CardItem.tsx
+++ front-onsekaz/src/components/CardItem.tsx
@@ -1,30 +1,25 @@
 import { AnnonceT } from '../types/AnnonceType';
 import CarouselComponent from './Carousel';
+import { Link } from 'react-router-dom';
 
 type CardProps = {
   annonce: AnnonceT;
 };
 
 const CardItem: React.FC<CardProps> = ({ annonce }) => (
-<div>
-  <a href={"annonce/"+annonce.id}>
-    <div className="card w-96 h-96  bg-base-300 shadow-xl">
-      <figure>
-        <CarouselComponent images={annonce.photos} />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title underline-offset-0">
-          {annonce.title}
-          <div className="badge badge-secondary">NEW</div>
-        </h2>
-        <div className="card-actions flex flex-grow">
-          <div className="badge badge-outline"><strong>Prix:</strong> {annonce.price.toString()} €</div>
-          <div className="badge badge-outline"><strong>Maximum de clients:</strong> {annonce.max_customer.toString()}</div>
+      <div className="card h-96 bg-base-300 shadow-xl">
+        <figure className="px-0.5 pt-0.5">
+          <CarouselComponent images ={annonce.photos}/>
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">{annonce.title}!</h2>
+          <div className="card-actions">
+            <Link to={`/annonce/${annonce.id}`} className="btn btn-primary">
+              Voir plus
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-  </a>
-</div>
 
 );
 
